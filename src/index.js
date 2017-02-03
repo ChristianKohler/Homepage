7fbfa18a8b1a49132fec15831d9332c1d7f1c6ba
chris <chris@chriss-MacBook.local>
2017-02-03 23:34:08 +0100

    changed service worker path

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ init();
 if (process.env.NODE_ENV === 'production') {
 	// cache all assets if browser supports serviceworker
 	if ('serviceWorker' in navigator && location.protocol === 'https:') {
-		navigator.serviceWorker.register('/service-worker.js');
+		navigator.serviceWorker.register('service-worker.js');
 	}
 } else {
 	// use preact's devtools
